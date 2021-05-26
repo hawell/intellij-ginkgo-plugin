c56d2b949b32be8dc56c4882302f10cc1e2de812
arash kordi <a.kordi@alibaba.ir>
2021-05-26 12:48:59 +0430

    add gutter icon for Describe and It

--- src/main/java/com/chordsoft/extensions/GinkgoLineMarkerProvider.java
+++ src/main/java/com/chordsoft/extensions/GinkgoLineMarkerProvider.java
@@ -1,13 +1,25 @@
 package com.chordsoft.extensions;
 
+import com.goide.psi.GoReferenceExpression;
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
 import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.editor.markup.GutterIconRenderer;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import org.jetbrains.annotations.NotNull;
+
 
 public class GinkgoLineMarkerProvider implements LineMarkerProvider {
     @Override
     public LineMarkerInfo<?> getLineMarkerInfo(@NotNull PsiElement element) {
+        Class<? extends PsiElement> e = element.getClass();
+        Class<? extends PsiElement> p = element.getParent().getClass();
+        if (element instanceof LeafPsiElement && element.getParent() instanceof GoReferenceExpression) {
+            if (element.textMatches("Describe") || element.textMatches("It")) {
+                return new LineMarkerInfo<>(element, element.getTextRange(), AllIcons.RunConfigurations.TestState.Run, null, null, GutterIconRenderer.Alignment.CENTER);
+            }
+        }
         return null;
     }
 }
